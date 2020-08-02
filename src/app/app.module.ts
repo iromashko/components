1ff302407204217fb21073abbb619684e455a08f
iromashko <iromashko@me.com>
2020-08-02 14:53:18 +0300

    youtube component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { YoutubePageComponent } from './youtube-page/youtube-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonsComponent
+    ButtonsComponent,
+    YoutubePageComponent
   ],
   imports: [
     BrowserModule,
